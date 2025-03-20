36a2be2503c10cf184230aaa7dadbf49c3587601
tykkor22 <tykkor22@gmail.com>
2025-03-20 11:48:10 +0900

    [kty] remember-me 설정

--- mvc/src/main/java/com/gn/mvc/security/WebSecurityConfig.java
+++ mvc/src/main/java/com/gn/mvc/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.gn.mvc.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration // 환경설정 파일
 @EnableWebSecurity // 스프링 시큐리티 사용 여부
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+	
+	private final DataSource dataSource;
 	
 	// 요청중에 정적 리소스가 있는 경우 -> Security 비활성화
 	@Bean
@@ -34,10 +43,24 @@ public class WebSecurityConfig {
 		.formLogin(login -> login.loginPage("/login")
 								.successHandler(new MyLoginSuccessHandler())
 								.failureHandler(new MyLoginFailureHandler()))
-		.logout(logout -> logout.clearAuthentication(true)
+		.logout(logout -> logout.logoutUrl("/logout")
+								.clearAuthentication(true)
 								.logoutSuccessUrl("/login")
-								.invalidateHttpSession(true));
+								.invalidateHttpSession(true)
+								.deleteCookies("remember-me"))
+		.rememberMe(rememberMe -> rememberMe.rememberMeParameter("remember-me")
+											.tokenValiditySeconds(60*60*24*30)
+											.alwaysRemember(false)
+											.tokenRepository(tokenRepository()));
 		return http.build();
+	}
+	
+	// 데이터베이스 접근 Bean 등록
+	@Bean
+	PersistentTokenRepository tokenRepository() {
+		JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+		jdbcTokenRepository.setDataSource(dataSource);
+		return jdbcTokenRepository;
 	}
 	
 	// 비밀번호 암호화에 사용될 Bean 등록
