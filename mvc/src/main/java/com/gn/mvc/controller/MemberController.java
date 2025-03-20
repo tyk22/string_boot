2418a4ab802bb0adf789c3c8f51d5f3d75dd1883
tykkor22 <tykkor22@gmail.com>
2025-03-20 14:25:05 +0900

    [kty] 회정 정보 수정

--- mvc/src/main/java/com/gn/mvc/controller/MemberController.java
+++ mvc/src/main/java/com/gn/mvc/controller/MemberController.java
@@ -6,19 +6,56 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.entity.Member;
 import com.gn.mvc.service.MemberService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 	private final MemberService service;
+	
+	
+	@PostMapping("/member/{id}/update")
+	@ResponseBody
+	public Map<String, String> memberUpdateApi(MemberDto param,
+			HttpServletResponse response){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원 수정중 오류가 발생하였습니다.");
+		
+		int result = service.updateMember(param);
+		if(result >0) {
+			// 어쩔 수 없이 비지니스 로직을 여기서 써야함
+			// 쿠키(remember-me) 무효화
+			Cookie rememberMe = new Cookie("remember-me",null);
+			rememberMe.setMaxAge(0);
+			rememberMe.setPath("/");
+			response.addCookie(rememberMe);
+			
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "회원이 수정되었습니다.");
+		}
+		
+		return resultMap;
+	}
+	
+	
+	@GetMapping("/member/{id}/update")
+	public String memberUpdateView(@PathVariable("id") Long id, Model model) {
+		Member member = service.selectMemberOne(id);
+		model.addAttribute("member",member);
+		return "member/update";
+	}
 	
 	@GetMapping("/login")
 	public String loginView(
--- mvc/src/main/java/com/gn/mvc/service/MemberService.java
+++ mvc/src/main/java/com/gn/mvc/service/MemberService.java
@@ -1,5 +1,13 @@
 package com.gn.mvc.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +23,59 @@ public class MemberService {
 	
 	private final MemberRepository repository;
 	private final PasswordEncoder passwordEncoder;
+	private final DataSource dataSource;
+	private final UserDetailsService userDetailsService;
+	// 1. 반환형 : int 
+	// 2. 메소드명 : updateMember
+	// 3. 매개변수 : MemberDto ( param )
+	// 4. 역할 
+	public int updateMember(MemberDto param) {
+		int result=0;
+		try {
+			// (1) 데이터베이스 회원 정보 수정
+			param.setMember_pw(passwordEncoder.encode(param.getMember_pw()));
+			Member updated = repository.save(param.toEntity());
+			if(updated != null) {
+				// 여기선 DB , Controller에서 cookie
+				// (2) remember-me(**DB**, cookie)가 있다면 무효화
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM persistent_logins WHERE username = ?";
+				jdbcTemplate.update(sql,param.getMember_id());
+				// (3) 변경된 회원 정보 Security Context에 즉시 반영
+				UserDetails updatedUserDetails
+					=  userDetailsService.loadUserByUsername(param.getMember_id());
+				Authentication newAuth = new UsernamePasswordAuthenticationToken(
+						updatedUserDetails,
+						updatedUserDetails.getPassword(),
+						updatedUserDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(newAuth);
+				
+				result = 1;
+			}
+			
+			
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
 	
+	
+	
+	
+	
+	// 1. 반환형 : Member
+	// 2. 메소드명 : selectMemberOne
+	// 3. 매개변수 : 전달받은 값 ( 자료형 ) 
+	// 4. 역할 : PK값 기준 회원 정보 단일 조회 후 반환
+	public Member selectMemberOne(Long id) {
+		// Member member = repository.findById(id).orElse(null);
+		// return member;
+		
+		return repository.findById(id).orElse(null);
+	}
 	public MemberDto createMember(MemberDto dto) {
 		
 		
@@ -27,5 +87,6 @@ public class MemberService {
 		
 		return new MemberDto().toDto(result);
 	}
+	
 	
 }
